80fbe737bda234c3ce50e20886b3570233d0cc43
987456987 <donufrak112697@gmail.com>
2023-06-02 23:40:38 -0700

    email validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
 import './App.css'
-import iconList from './assets/images/icon-list.svg';
-import desktopImg from './assets/images/illustration-sign-up-desktop.svg';
-import mobileImg from './assets/images/illustration-sign-up-mobile.svg';
+import iconList from './assets/images/icon-list.svg'
+import desktopImg from './assets/images/illustration-sign-up-desktop.svg'
+import mobileImg from './assets/images/illustration-sign-up-mobile.svg'
 
 export default function App() {
- 
+
+  
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    const emailInput = document.getElementById("email");
+    const emailError = document.getElementById("error");
+  
+    if (emailInput.validity.valid) {
+      emailInput.classList.remove('error-input')
+      emailError.classList.remove('error-text-display')
+      e.target.reset();
+    } else {
+      emailInput.classList.add('error-input')
+      emailError.classList.add('error-text-display')
+    }
+  }
+
   return (
     <div className='App'>
       <div className='container'>
@@ -27,8 +45,11 @@ export default function App() {
               <p>And much more!</p>
             </li>
           </ul>
-          <form>
-            <label htmlFor="email">Email address</label>
+          <form onSubmit={handleSubmit} id='form' noValidate>
+            <div className='input-labels'>
+              <label htmlFor="email">Email address</label>
+              <label id='error' className='error-text'>Valid email required</label>
+            </div>
             <input type='email' id='email' placeholder='email@company.com'/>
             <button>Subscribe to monthly newsletter</button>
           </form>
