5f69a81db469f81ba3e747bbeb22d0028d5f4818
987456987 <donufrak112697@gmail.com>
2023-06-03 10:23:33 -0700

    SuccessComponent functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import FormComponent from './FormComponent';
 import ListComponent from './ListComponent';
 import SuccessComponent from './SuccessComponent';
-import desktopImg from './assets/images/illustration-sign-up-desktop.svg'
-import mobileImg from './assets/images/illustration-sign-up-mobile.svg'
+import desktopImg from './assets/images/illustration-sign-up-desktop.svg';
+import mobileImg from './assets/images/illustration-sign-up-mobile.svg';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
 
   return (
     <div className='App'>
       <div className='main'>
-        <div className='container'>
-          <img className='mobile-img' src={mobileImg}/>
+        <div className='container' id="container">
+          <img className='mobile-img' src={mobileImg} />
           <div className='form-container'>
             <h1>Stay updated!</h1>
             <p style={{ padding: '1.5em 0' }}>Join 60,000+ product managers receiving monthly updates on:</p>
             <ListComponent />
-            <FormComponent />
+            <FormComponent onInputChange={handleInputChange} />
           </div>
-          <img className='desktop-img' src={ desktopImg } alt=''/>
+          <img className='desktop-img' src={desktopImg} alt='' />
         </div>
-        <SuccessComponent />
+        <SuccessComponent inputValue={inputValue} />
       </div>
     </div>
-  )
+  );
 }
-
--- src/SuccessComponent.jsx
+++ src/SuccessComponent.jsx
@@ -1,15 +1,23 @@
 import iconList from './assets/images/icon-list.svg'
 
-export default function SuccessComponent() {
+export default function SuccessComponent({ inputValue }) {
+    const success = document.getElementById('success')
+    const container = document.getElementById('container')
+
+    const handleClick = () => {
+        success.classList.remove("container-block")
+        container.classList.remove("container-hidden")
+    }
+
      return (
-        <div className='success-container'>
-            <img src={iconList} width={'65px'} style={{marginBottom: '3em'}}/>
+        <div className='success-container' id="success">
+            <img src={iconList} width={'65px'} style={{marginBottom: '2em'}}/>
             <h1>Thanks for subscribing!</h1>
             <p style={{ padding: '1.5em 0' }}>
-                A confirmation email has been sent to ash@loremcompany.com. 
+                A confirmation email has been sent to <b>{inputValue}</b>. 
                 Please open it and click the button inside to confirm your subscription.
             </p>
-            <button>Dismiss message</button>
+            <button onClick={handleClick} >Dismiss message</button>
       </div>
      )
 }
